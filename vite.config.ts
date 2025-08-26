e9e65b75a16e95b610bf3c414dc1cac5a84f0201
alunoenzoye <enzo.eto.senai404@gmail.com>
2025-08-26 14:33:55 -0300

    feat: add react svgr

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+import svgr from "vite-plugin-svgr";
 
 export default defineConfig({
     plugins: [
@@ -13,6 +14,7 @@ export default defineConfig({
         }),
         react(),
         tailwindcss(),
+        svgr()
     ],
     esbuild: {
         jsx: 'automatic',
