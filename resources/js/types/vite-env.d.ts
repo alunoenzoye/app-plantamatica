418300ec4c7bbb6ceb3d13de422a03c82347b9bd
alunoenzoye <enzo.eto.senai404@gmail.com>
2025-08-26 14:39:53 -0300

    feat: use logo

--- resources/js/types/vite-env.d.ts
+++ resources/js/types/vite-env.d.ts
@@ -1,3 +1,3 @@
 /// <reference types="vite/client" />
 /// <reference types="../../types/generated.d.ts" />
-//  <reference types="vite-plugin-svgr/client" />
+///  <reference types="vite-plugin-svgr/client" />
