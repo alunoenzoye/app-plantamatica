5ae2c4466a4570e0127703a42861d9027e438872
alunoenzoye <enzo.eto.senai404@gmail.com>
2025-09-23 15:52:49 -0300

    feat: update checkbox instantly

--- resources/js/components/task-list.tsx
+++ resources/js/components/task-list.tsx
@@ -3,6 +3,7 @@ import { router } from '@inertiajs/react';
 import { ColumnDef, createColumnHelper } from '@tanstack/react-table';
 import { formatDate } from 'date-fns';
 import { Checkbox } from './ui/checkbox';
+import { useMemo, useState } from 'react';
 
 const priorityStyles = {
     low: {
@@ -21,82 +22,79 @@ const priorityStyles = {
 
 const columnHelper = createColumnHelper<App.Data.TaskData>()
 
-const columns: ColumnDef<App.Data.TaskData>[] = [
-    // {
-    //     accessorKey: "id",
-    //     header: "ID"
-    // },
-    columnHelper.display({
-        id: "complete_checkbox",
-        cell: ((props) => {
-            const done = props.row.original.done;
-
-            return (<Checkbox
-                className="hover:cursor-pointer p-0"
-                checked={done}
-                onCheckedChange={() => {
-                    // TODO: update value instantly for feedback
-                    router.patch(route(
-                        'tasks.complete', props.row.original.id),
-                        {
-                            done: !props.row.original.done
-                        }
-                    );
-                    router.reload();
-                }}
-            />)
-        }),
-
-        // cell: ({ row }) => (
-        //     <Checkbox
-        //         checked={row.getIsSelected()}
-        //         onCheckedChange={(value) => row.toggleSelected(!!value)}
-        //         aria-label="Select row"
-        //     />
-        // ),
-        enableSorting: false,
-        enableHiding: false,
-    }),
-    {
-        accessorKey: "name",
-        header: "Nome"
-    },
-    {
-        accessorKey: "priority",
-        header: "Prioridade",
-        cell: ({ row }) => {
-            const priorityStyle = priorityStyles[row.getValue("priority") as keyof typeof priorityStyles];
-
-            return <span className={priorityStyle.className}>{priorityStyle.text}</span>
-        }
-    },
-    {
-        accessorKey: "due_date",
-        header: "Prazo",
-        cell: ({ row }) => {
-            const date: Date = row.getValue("due_date");
-
-            if (date) {
-                return formatDate(date, "dd/MM/yyyy")
-            } else {
-                return null;
-            }
-        }
-    },
-    // {
-    //     accessorKey: "name",
-    //     header: "Nome"
-    // },
-]
 
 interface taskListProps {
     tasks: App.Data.TaskData[],
 }
 
 export default function TaskList({ tasks }: taskListProps) {
+    const [taskData, setTaskData] = useState(tasks);
+
+    const columns = useMemo(() => {
+        const columns: ColumnDef<App.Data.TaskData>[] = [
+            columnHelper.display({
+                id: "complete_checkbox",
+                cell: ((props) => {
+                    const done = props.row.original.done;
+
+                    return (<Checkbox
+                        className="hover:cursor-pointer p-0"
+                        checked={done}
+                        onCheckedChange={() => {
+                            const toggle = !props.row.original.done
+
+                            const cloned = [...taskData]
+                            const row = cloned.at(props.row.index)
+                            if (row) row.done = toggle
+
+                            setTaskData(cloned)
+
+                            router.patch(route(
+                                'tasks.complete', props.row.original.id),
+                                {
+                                    done: toggle
+                                }
+                            );
+                        }}
+                    />)
+                }),
+                enableSorting: false,
+                enableHiding: false,
+            }),
+            {
+                accessorKey: "name",
+                header: "Nome"
+            },
+            {
+                accessorKey: "priority",
+                header: "Prioridade",
+                cell: ({ row }) => {
+                    const priorityStyle = priorityStyles[row.getValue("priority") as keyof typeof priorityStyles];
+
+                    return <span className={priorityStyle.className}>{priorityStyle.text}</span>
+                }
+            },
+            {
+                accessorKey: "due_date",
+                header: "Prazo",
+                cell: ({ row }) => {
+                    const date: Date = row.getValue("due_date");
+
+                    if (date) {
+                        return formatDate(date, "dd/MM/yyyy")
+                    } else {
+                        return null;
+                    }
+                }
+            },
+        ]
+
+        return columns
+    }, []);
+
     return (
-        <DataTable columns={columns} data={tasks} columnStyleArray={[
+        <DataTable columns={columns} data={taskData} columnStyleArray={[
             "w-8"
-        ]}/>
+        ]} />
     )
 }
