3bf445a54427756e644eb0ba7aa0eda33d998412
alunoenzoye <enzo.eto.senai404@gmail.com>
2025-09-25 13:55:16 -0300

    fix: add missing HasRoles trait

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,9 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
+
+// use Spatie\Permission\Traits\HasRoles;
 
 /**
  * @property int $id
@@ -41,7 +44,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, SoftDeletes;
+    use HasFactory, Notifiable, SoftDeletes, HasRoles;
 
     /**
      * The attributes that are mass assignable.
